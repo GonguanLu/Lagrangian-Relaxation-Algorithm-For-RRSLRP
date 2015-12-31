df9ed33d688379d2ff79e1d2c2e9e2aec5d60be2
Gongyuan.Lu <lugongyuan@qq.com>
2015-12-31 20:19:52 +0800

    Create stdafx.h

---
+++ stdafx.h
@@ -0,0 +1,26 @@
+
+#pragma once
+
+#include "targetver.h"
+
+#include <stdio.h>
+#include <tchar.h>
+#define _ATL_CSTRING_EXPLICIT_CONSTRUCTORS      
+
+#ifndef VC_EXTRALEAN
+#define VC_EXTRALEAN            
+#endif
+
+#include <afx.h>
+#include <afxwin.h>      
+#include <afxext.h>      
+#ifndef _AFX_NO_OLE_SUPPORT
+#include <afxdtctl.h>           
+#endif
+#ifndef _AFX_NO_AFXCMN_SUPPORT
+#include <afxcmn.h>                    
+#endif
+
+#include <iostream>
+
+
